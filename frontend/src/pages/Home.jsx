f7143d16879ad31bf11c1fbd79c1dc15227ed839
anusha7530 <agarwalanusha13@gmail.com>
2024-05-04 00:06:47 +0530

    Developed landing page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,67 @@
-import React from 'react'
+import React from "react";
+import heroImg01 from "../assets/images/hero-img01.png";
+import heroImg02 from "../assets/images/hero-img02.png";
+import heroImg03 from "../assets/images/hero-img03.png";
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
-  )
-}
+    <>
+      <section className="hero_section pt-[60px] 2xl:h-[800px]">
+        <div className="container">
+          <div className="flex flex-col lg:flex-row gap-[90px] items-center justify-between">
+            <div>
+              <div className="lg:w-[570px]">
+                <h1 className="text-[36px] leading-[46px] text-headingColor font-[800] md:text-[50px] md:leading-[70px]">
+                  Book Your Next Doctor Appointment Online Now!
+                </h1>
+                <p className="text_para">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Facilis aspernatur repudiandae eaque aperiam dolor recusandae
+                  necessitatibus voluptatibus accusamus ratione delectus!
+                </p>
 
-export default Home
-Home
+                <button className="btn">Request an Appointment</button>
+              </div>
+
+              <div className="mt-[30px] lg:mt-[70px] flex flex-col lg:flex-row lg:items-center gap-5 lg:gap-[30px]">
+                <div>
+                  <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor">
+                    30+
+                  </h2>
+                  <span className="w-[100px] h-2 bg-yellowColor rounded-full block mt-[-14px]"></span>
+                  <p className="text_para">Years of Experience</p>
+                </div>
+                <div>
+                  <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor">
+                    15+
+                  </h2>
+                  <span className="w-[100px] h-2 bg-purpleColor rounded-full block mt-[-14px]"></span>
+                  <p className="text_para">Clinic Locations</p>
+                </div>
+                <div>
+                  <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor">
+                    100%
+                  </h2>
+                  <span className="w-[100px] h-2 bg-irisBlueColor rounded-full block mt-[-14px]"></span>
+                  <p className="text_para">Patience Satisfaction</p>
+                </div>
+              </div>
+            </div>
+            <div className="flex gap-[30px] justify-end">
+              <div>
+                <img src={heroImg01} alt="home" />
+              </div>
+              <div className="mt-[30px]">
+                <img src={heroImg02} alt="home2" className="w-full mb-[30px]"/>
+                <img src={heroImg03} alt="home3" className="w-full"/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Home;
+Home;
