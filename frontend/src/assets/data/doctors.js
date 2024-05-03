26b636e62438050a556de2f25316325a5dfce1fa
anusha7530 <agarwalanusha13@gmail.com>
2024-05-03 20:14:59 +0530

    Added assets

---
+++ frontend/src/assets/data/doctors.js
@@ -0,0 +1,36 @@
+import doctor1 from "../images/doctor1.avif"
+import doctor2 from "../images/doctor2.jpg";
+import doctor3 from "../images/doctor3.jpg";
+
+export const doctors = [
+  {
+    id: "01",
+    name: "Dr. Emily Chen",
+    specialty: "Cardiology",
+    avgRating: 4.5,
+    totalRating: 120,
+    photo: doctor1,
+    totalPatients: 1000,
+    hospital: "Mercy Hospital, Metropolis",
+  },
+  {
+    id: "02",
+    name: "Dr. Michael Patel",
+    specialty: "Dermatology",
+    avgRating: 4.8,
+    totalRating: 292,
+    photo: doctor2,
+    totalPatients: 1000,
+    hospital: "Lakeside Medical Center,Hopeville",
+  },
+  {
+    id: "03",
+    name: "Dr. David Lee",
+    specialty: "Orthopedics",
+    avgRating: 4.6,
+    totalRating: 231,
+    photo: doctor3,
+    totalPatients: 1000,
+    hospital: "Summit Orthopedic Clinic, Summit",
+  },
+];
---
+++ frontend/src/assets/data/faq.js
@@ -0,0 +1,27 @@
+export const faqs = [
+    {
+      question: "How does HealthSpot work?",
+      content:
+        "HealthSpot is a user-friendly application that allows you to search for and book appointments with healthcare providers. Simply open the webiste, create an account, search for a doctor or specialist, select a convenient time slot, and book your appointment seamlessly.",
+    },
+    {
+      question: "Can I see my doctor's availability in real-time?",
+      content:
+        "Yes, HealthSpot provides real-time availability for doctors and healthcare providers listed on the platform. You can view available appointment slots and choose the one that best fits your schedule.",
+    },
+    {
+      question: "What types of healthcare professionals can I book appointments with using HealthSpot?",
+      content:
+        "HealthSpot offers a wide range of healthcare professionals, including general practitioners, specialists, dentists, psychologists, chiropractors, and more. You can search for providers based on their specialty, location, and availability.",
+    },
+    {
+      question: "Is my personal information secure on HealthSpot?",
+      content:
+        "Yes, HealthSpot takes your privacy and security seriously. We use industry-standard encryption protocols to protect your personal information and ensure that your data remains secure at all times.",
+    },
+    {
+      question: "Are there any fees associated with using HealthSpot?",
+      content:
+        "HealthSpot is free to download and use for booking appointments. However, some healthcare providers may charge consultation fees or other service fees for appointments booked through the platform. Any applicable fees will be clearly outlined before you confirm your appointment booking.",
+    },
+  ];
