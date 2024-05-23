69290ae4016610d42201c8ed83d20ac31b02406a
anusha7530 <agarwalanusha13@gmail.com>
2024-05-24 02:33:34 +0530

    Login route and controller

--- backend/Controllers/authController.js
+++ backend/Controllers/authController.js
@@ -3,9 +3,15 @@ import Doctor from "../models/DoctorSchema.js";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 
-// const generateToken = user => {
-//     return jwt.sign({id:user._id, role:user.role, process.env.JWT_SECRET_key})
-// }
+const generateToken = (user) => {
+  return jwt.sign(
+    { id: user._id, role: user.role },
+    process.env.JWT_SECRET_KEY,
+    {
+      expiresIn: "15D",
+    }
+  );
+};
 
 export const register = async (req, res) => {
   const { email, password, name, role, photo, gender } = req.body;
@@ -67,27 +73,47 @@ export const register = async (req, res) => {
       .json({ success: false, message: "Internal server error, Try again!" });
   }
 };
+
 export const login = async (req, res) => {
-    const {email,password} = req.body;
+  const { email } = req.body;
 
   try {
     let user = null;
-    const patient = await User.findOne({email});
-    const doctor = await Doctor.findOne({email});
+    const patient = await User.findOne({ email });
+    const doctor = await Doctor.findOne({ email });
 
-    if(patient){
-        user = patient;
+    if (patient) {
+      user = patient;
     }
-    if(doctor){
-        user = doctor;
+    if (doctor) {
+      user = doctor;
     }
     if (!user) {
-        return res.status(404).json({ message: "Invalid credentials" });
+      return res
+        .status(404)
+        .json({message: "Invalid credentials" });
     }
-    const isPasswordMatch = bcrypt.compare(password, user.password);
+    const isPasswordMatch = await bcrypt.compare(req.body.password, user.password);
 
-    if(!isPasswordMatch){
-        return res.status(400).json({ message: "Invalid User credentials" });
+    if (!isPasswordMatch) {
+      return res
+        .status(400)
+        .json({ status: false, message: "Invalid User credentials" });
     }
-  } catch (error) {}
+
+    const token = generateToken(user);
+
+    const { password, role, appointment, ...rest } = user._doc;
+    res
+      .status(200)
+      .json({
+        status: true,
+        message: "Successfully login",
+        token,
+        data: { ...rest },
+        role,
+      });
+  } catch (error) {
+    res.status(500).json({ status: false, message: "Failed to login" });
+  }
 };
