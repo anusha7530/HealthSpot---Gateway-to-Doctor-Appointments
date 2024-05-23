0ccabd0363a7039910d5cf4b504b67d752088ab1
anusha7530 <agarwalanusha13@gmail.com>
2024-05-24 02:50:37 +0530

    Get users

--- backend/Controllers/userController.js
+++ backend/Controllers/userController.js
@@ -8,13 +8,11 @@ export const updateUser = async (req, res) => {
       { $set: req.body },
       { new: true }
     );
-    res
-      .status(200)
-      .json({
-        success: true,
-        message: "Successfully updated",
-        data: updateUser,
-      });
+    res.status(200).json({
+      success: true,
+      message: "Successfully updated",
+      data: updateUser,
+    });
   } catch (error) {
     res.status(500).json({ success: false, message: "Failed to update" });
   }
@@ -23,16 +21,39 @@ export const updateUser = async (req, res) => {
 export const deleteUser = async (req, res) => {
   const id = req.params.id;
   try {
-    await User.findByIdAndDelete(
-      id
-    );
-    res
-      .status(200)
-      .json({
-        success: true,
-        message: "Successfully deleted",
-      });
+    await User.findByIdAndDelete(id);
+    res.status(200).json({
+      success: true,
+      message: "Successfully deleted",
+    });
   } catch (error) {
     res.status(500).json({ success: false, message: "Failed to delete" });
+  }
+};
+
+export const getSingleUser = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const user = await User.findById(id);
+    res.status(200).json({
+      success: true,
+      message: "User found",
+      data: user,
+    });
+  } catch (error) {
+    res.status(404).json({ success: false, message: "No user found" });
+  }
+};
+
+export const getAllUser = async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).json({
+      success: true,
+      message: "Users found",
+      data: users,
+    });
+  } catch (error) {
+    res.status(404).json({ success: false, message: "Not found" });
   }
 };
