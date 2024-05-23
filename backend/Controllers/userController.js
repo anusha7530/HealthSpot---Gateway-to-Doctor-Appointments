e240cdeeae5c8600603db73b2693443cb10da7c7
anusha7530 <agarwalanusha13@gmail.com>
2024-05-24 02:38:32 +0530

    Update User controller

---
+++ backend/Controllers/userController.js
@@ -0,0 +1,21 @@
+import User from "../models/UserSchema.js";
+
+export const updateUser = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      id,
+      { $set: req.body },
+      { new: true }
+    );
+    res
+      .status(200)
+      .json({
+        success: true,
+        message: "Successfully updated",
+        data: updateUser,
+      });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Failed to update" });
+  }
+};
