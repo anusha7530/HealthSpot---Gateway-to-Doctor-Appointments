a6bca774287a834c827a893527924a999e5de79e
anusha7530 <agarwalanusha13@gmail.com>
2024-05-25 02:43:38 +0530

    Implement Review functionality

--- backend/Routes/doctor.js
+++ backend/Routes/doctor.js
@@ -6,8 +6,11 @@ import {
   getSingleDoctor,
 } from "../Controllers/DoctorController.js";
 import { authenticate, restrict } from "../auth/verifyToken.js";
+import reviewRouter from "./review.js";
 
 const router = express.Router();
+
+router.use('/:doctorId/reviews',reviewRouter);
 
 router.get("/:id", getSingleDoctor);
 router.get("/", getAllDoctor);
