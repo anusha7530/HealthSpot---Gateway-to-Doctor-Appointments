9a641dd5faf1e9c5529ca83cd8b4411e84078ec5
Anusha Agarwal <agarwalanusha13@gmail.com>
2024-06-26 00:53:30 +0530

    Added Bookings UI

---
+++ frontend/src/components/Error.jsx
@@ -0,0 +1,12 @@
+/*eslient-disable react/prop-types */
+
+const Error = ({errorMessage}) => {
+  return (
+    <div className="flex items-center justify-center w-full h-full">
+    <h3 className="text-headingColor text-[20px] leading[30px] fonr-semibold">{errorMessage}</h3>
+      
+    </div>
+  )
+}
+
+export default Error
