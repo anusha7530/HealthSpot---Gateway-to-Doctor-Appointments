893024de9705e8c4c285138553c5b30372c9754a
Anusha Agarwal <agarwalanusha13@gmail.com>
2024-06-26 01:10:06 +0530

    Added profile edit page

--- frontend/src/Dashboard/user-account/MyBookings.jsx
+++ frontend/src/Dashboard/user-account/MyBookings.jsx
@@ -5,26 +5,32 @@ import Loading from "../../components/Loading";
 import Error from "../../components/Error";
 
 const MyBookings = () => {
-    const {data:appointments,loading,error} = useFetchData(`${BASE_URL}/users/appointments/my-appointments`)
+  const {
+    data: appointments,
+    loading,
+    error,
+  } = useFetchData(`${BASE_URL}/users/appointments/my-appointments`);
 
   return (
     <div>
-      {loading && !error && <Loading/>}
-      {error && !loading && <Error/>}
+      {loading && !error && <Loading />}
+      {error && !loading && <Error />}
 
-      {!loading && !error &&(
-    <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
-        {appointments.map(doctor => (
-            <DoctorCard doctor={doctor} key={doctor._id}/>
-        ))}
-    </div>
+      {!loading && !error && (
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
+          {appointments.map((doctor) => (
+            <DoctorCard doctor={doctor} key={doctor._id} />
+          ))}
+        </div>
       )}
 
       {!loading && !error && appointments.length === 0 && (
-        <h2 className="mt-5 text-center leading-7 text-[20px] font-semibold text-primaryColor">You have not booked any doctor yet.</h2>
+        <h2 className="mt-5 text-center leading-7 text-[20px] font-semibold text-primaryColor">
+          You have not booked any doctor yet.
+        </h2>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default MyBookings
+export default MyBookings;
