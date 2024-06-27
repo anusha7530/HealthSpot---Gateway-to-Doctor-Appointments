6bf5cb8b3162db9193fb4842468f3ffc10180ef4
Anusha Agarwal <agarwalanusha13@gmail.com>
2024-06-28 00:54:20 +0530

    Added tabs in Doctor's dashboard

--- frontend/src/Dashboard/doctor-account/Dashboard.jsx
+++ frontend/src/Dashboard/doctor-account/Dashboard.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useState } from "react";
+import Loader from "../../components/Loading";
+import Error from "../../components/Error";
+import useGetProfile from "../../hooks/useFetchData";
+import { BASE_URL } from "../../config";
+import Tabs from "./Tabs";
 
 const Dashboard = () => {
+  const { data, loading, error } = useGetProfile(
+    `${BASE_URL}/doctors/profile/me`
+  );
+  const [tab,setTab] = useState('overview');
   return (
-    <div>
-      dash
-    </div>
-  )
-}
+    <section>
+      <div className="max-w-[1170px] px-5 mx-auto">
+        {loading && !error && <Loader />}
+        {error && !loading && <Error />}
+        {
+          !loading && !error && (
+            <div className="grid lg:grid-cols-3 gap-[30px] lg:gap-[50px] ">
+              <Tabs tab={tab} setTab={setTab}/>
+            </div>
+          )
+        }
+      </div>
+    </section>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
